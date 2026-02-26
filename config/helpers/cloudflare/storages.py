0a04f4db853542c464bad3ba39a33f27823dc65c
hassaanalansary <hassaanalansary@yahoo.com>
2026-02-26 18:28:35 +0200

    fix: ManifestStaticFilesStorage urls

--- config/helpers/cloudflare/storages.py
+++ config/helpers/cloudflare/storages.py
@@ -4,8 +4,10 @@ from storages.backends.s3 import S3ManifestStaticStorage, S3Storage
 class StaticFileStorage(S3ManifestStaticStorage):
     location = "static"
     default_acl = "public-read"
+    querystring_auth = False
 
 
 class MediaFileStorage(S3Storage):
     location = "media"
     default_acl = "public-read"
+    querystring_auth = False
