7837bca4fc5d4b3d893a87b80894eb2da3148640
hassaanalansary <hassaanalansary@yahoo.com>
2026-05-17 10:11:54 +0300

    fix: get mixpanel url dynamically

--- apps/usage_tracking/api/portal/usage.py
+++ apps/usage_tracking/api/portal/usage.py
@@ -2,12 +2,12 @@
 
 from __future__ import annotations
 
-from django.conf import settings
 from ninja import Schema
 
 from apps.core.ninja_utils.request import Request
 from apps.core.ninja_utils.router import ItqanRouter
 from apps.core.ninja_utils.tags import NinjaTag
+from apps.publishers.models import Publisher
 
 router = ItqanRouter(tags=[NinjaTag.USAGE])
 
@@ -21,5 +21,5 @@ class BoardUrlOut(Schema):
     response=BoardUrlOut,
     description="Get the Mixpanel board URL for the current user",
 )
-def get_usage_board_url(request: Request) -> BoardUrlOut:
-    return BoardUrlOut(board_url=getattr(settings, "MIXPANEL_MAIN_BOARD_URL", None))
+def get_usage_board_url(request: Request) -> str | None:
+    return Publisher.objects.order_by("id").first().mixpanel_board_url
