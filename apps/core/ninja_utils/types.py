0c33db28b2ef997bac834f46526b3200cac47c16
hassaanalansary <hassaanalansary@yahoo.com>
2026-05-31 13:42:53 +0300

    fix: add annotation to AbsoluteUrl

--- apps/core/ninja_utils/types.py
+++ apps/core/ninja_utils/types.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, TypedDict
 
 from pydantic import PlainSerializer
 from pydantic_core.core_schema import SerializationInfo
@@ -6,14 +6,18 @@ from pydantic_core.core_schema import SerializationInfo
 from apps.core.ninja_utils.request import Request
 
 
-def _build_url(url: str, info: SerializationInfo) -> str:
+class SerializationInfoDict(TypedDict):
+    request: Request
+    response_status: int
+
+
+def _build_url(url: str, info: SerializationInfo[SerializationInfoDict]) -> str:
     if not url:
         return url
-    context = info.context
-    if not context or "request" not in context:
-        return url
-    request: Request = context["request"]
+    request = info.context["request"]
     return request.build_absolute_uri(url)
 
 
 AbsoluteUrl = Annotated[str, PlainSerializer(_build_url, return_type=str)]
+
+__all__ = [AbsoluteUrl]
